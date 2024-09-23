6ce6994015c234e5470ec6ef23af8e4aecfbfa7e
tomas-klaus <klaustomik@gmail.com>
2024-09-23 23:56:16 +0200

    ftsplit

--- lib_ft/ft_split.c
+++ lib_ft/ft_split.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tklaus <tklaus@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tomasklaus <tomasklaus@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:28:27 by tklaus            #+#    #+#             */
-/*   Updated: 2024/09/23 18:39:23 by tklaus           ###   ########.fr       */
+/*   Updated: 2024/09/23 23:55:51 by tomasklaus       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	countsegments(char const *s, char c)
 {
@@ -22,7 +22,6 @@ int	countsegments(char const *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		printf("%c", s[i]);
 		if (s[i] == c)
 			count++;
 		i++;
@@ -30,26 +29,79 @@ int	countsegments(char const *s, char c)
 	return (count);
 }
 
-char **ft_split(char const *s, char c){
-    char **arr;
-    int count;
-    
-    count = countsegments(s, c);
-    printf("%d\n",count);
+void	assignvalues(char **arr, int count, char const *s, char c)
+{
+	int	i;
+	int	j;
+	int	n;
 
-    
-    
+	i = 0;
+	j = 0;
+	n = 0;
+	while (i <= count)
+	{
+		j = 0;
+		while (s[n] && s[n] != c)
+		{
+			arr[i][j] = s[n];
+			j++;
+			n++;
+		}
+		i++;
+		n++;
+	}
+}
 
-    return arr;
+int	seglen(char const *s, char c, int start)
+{
+	int	i;
+	int	newstart;
+
+	newstart = start;
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		newstart++;
+		i++;
+	}
+	return (newstart);
+}
+
+void	innerarray(char **arr, int count, char const *s, char c)
+{
+	int	start;
+	int	i;
+	int	end;
+
+	start = 0;
+	i = 0;
+	while (i <= count)
+	{
+		end = seglen(s, c, start);
+		arr[i] = malloc((end - start) * sizeof(char));
+		i++;
+		start = end;
+	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	int		count;
+
+	count = countsegments(s, c);
+	arr = malloc((count + 1) * sizeof(char *));
+	innerarray(arr, count, s, c);
+	assignvalues(arr, count, s, c);
+	return (arr);
 }
 
 int	main(void)
 {
-	char const s[] = "hello, im, going, to, 42, school";
+	char const s[] = "henllo, im, going, to, 42 , school";
 	char c = ',';
-	printf("hello");
 	char **arr = ft_split(s, c);
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		printf("%s\n", arr[i]);
 		free(arr[i]);
