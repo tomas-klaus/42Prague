85f66624e018f49e0cb59156d36bdacfed3da4f7
tomas-klaus <klaustomik@gmail.com>
2025-03-01 11:41:04 +0100

    minitalk

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,13 +6,58 @@
 /*   By: tomasklaus <tomasklaus@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:31:17 by tomasklaus        #+#    #+#             */
-/*   Updated: 2025/01/03 18:26:25 by tomasklaus       ###   ########.fr       */
+/*   Updated: 2025/03/01 11:04:40 by tomasklaus       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 /* with each signal add either 0 or 1 to a string(?) and then convert and display it all together */
+
+char *str;
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptrdest;
+	unsigned char	*ptrsrc;
+
+	if (!src || !dest)
+		return (dest);
+	i = 0;
+	ptrdest = (unsigned char *)dest;
+	ptrsrc = (unsigned char *)src;
+	while (i < n)
+	{
+		ptrdest[i] = ptrsrc[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	add_to_str(char c)
+{
+	static int	i = 0;
+
+	char *new_str;
+
+	new_str = malloc(i + 2);
+	if (!new_str)
+		exit(1);
+	if (str)
+	{
+		
+		ft_memcpy(new_str, str, i);
+		free(str);
+	}
+	new_str[i] = c;
+	new_str[i + 1] = '\0';
+	str = new_str;
+	i++;
+	if (c == '\0')
+		i = 0;
+}
+
 void	signal_handler(int signum)
 {
 	static int	bit_count = 0;
@@ -23,9 +68,9 @@ void	signal_handler(int signum)
 	bit_count++;
 	if (bit_count == 8)
 	{
+		add_to_str(current_char);
 		if (current_char == '\0')
-			exit(0);
-		ft_printf("%c", current_char);
+			ft_printf("%s", str);
 		bit_count = 0;
 		current_char = 0;
 	}
