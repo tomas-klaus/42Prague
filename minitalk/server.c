1ee44fd25b3b9615d21acc1717955a8f73658ba5
Tomas Klaus <tklaus@c1r3s5.42prague.com>
2025-03-02 16:32:47 +0100

    leak hunt

--- minitalk/server.c
+++ minitalk/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomasklaus <tomasklaus@student.42.fr>      +#+  +:+       +#+        */
+/*   By: tklaus <tklaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:31:17 by tomasklaus        #+#    #+#             */
-/*   Updated: 2025/03/01 12:28:32 by tomasklaus       ###   ########.fr       */
+/*   Updated: 2025/03/02 16:31:17 by tklaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,28 @@ void	add_to_str(char c)
 	static int	i = 0;
 	char		*new_str;
 
-	new_str = malloc(i + 2);
+	new_str = malloc(i + 1);
 	if (!new_str)
 	{
-		free(g_str);
+		if (g_str)
+			free(g_str);
 		exit(1);
 	}
 	if (g_str)
 	{
 		ft_memcpy(new_str, g_str, i);
 		free(g_str);
+		g_str = NULL;
 	}
 	new_str[i] = c;
-	new_str[i + 1] = '\0';
 	g_str = new_str;
+	new_str = NULL;
 	i++;
 	if (c == '\0')
+	{
 		i = 0;
+		new_str = NULL;
+	}
 }
 
 void	signal_handler(int signum)
@@ -71,7 +76,11 @@ void	signal_handler(int signum)
 	{
 		add_to_str(current_char);
 		if (current_char == '\0')
+		{
 			ft_printf("%s", g_str);
+			free(g_str);
+			g_str = NULL;
+		}
 		bit_count = 0;
 		current_char = 0;
 	}
@@ -79,13 +88,14 @@ void	signal_handler(int signum)
 
 int	main(void)
 {
-	int	pid;
+	int pid;
 
+	g_str = NULL;
 	pid = getpid();
 	ft_printf("The process ID is %d\n", pid);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	while (1)
-		usleep(600);
+		usleep(300);
 	return (0);
 }
